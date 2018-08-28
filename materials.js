a7ff933fffbb6152e95b0f82c5e97e238f677a29
qubick <qubick.kim@gmail.com>
2018-08-27 19:50:21 -0600

    separate all materials

---
+++ materials.js
@@ -0,0 +1,5 @@
+var material = new THREE.MeshPhongMaterial(
+  { color: 0xA9A9A9, specular: 0x111111, shininess: 200, opacity:0.5 } );
+
+var faceColorMaterial = new THREE.MeshBasicMaterial(
+  { color: 0xffffff, vertexColors: THREE.FaceColors, opacity:0.5 } );
